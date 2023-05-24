d7c93ba12e104fe37190e4e7cd2c0a7ab5b9c7cd
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-05-24 21:43:21 +0500

    List-10

--- List10.py
+++ List10.py
@@ -6,4 +6,14 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+    i=0
+    k=0
+    s=0
+    while i<len(list1):
+        if list1[i]==1:
+            k+=1
+        elif list1[i]==0:
+            s+=1
+        i+=1
+    return [k,s]
+print(main([1, 0, 0, 0, 1, 0, 1, 0,1]))
